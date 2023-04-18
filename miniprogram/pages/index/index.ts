0254650f0a744f0c9894ea474b2f59b213307e58
ReginYuan <2496943860@qq.com>
2023-04-19 07:40:38 +0800

    feat: 修改对应数据

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -9,7 +9,13 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    swiperList:[
+      {
+        imageUrl:'../../assets/images/swiper-example.png',
+
+      }
+    ]
   },
   // 事件处理函数
   bindViewTap() {
